0591cef0c9632a9254506d91d9fcf650b7be1a67
Alperen DERİCİ <alperen.derici@std.izmirekonomi.edu.tr>
2022-01-12 20:28:58 +0300

    terminal prog ekran
    Co-Authored-By: KYakut35 <92581481+KYakut35@users.noreply.github.com>
    Co-Authored-By: cntrktzr <70589341+cntrktzr@users.noreply.github.com>
    Co-Authored-By: gizemhacioglu <64923120+gizemhacioglu@users.noreply.github.com>

--- src/projePack/Main.java
+++ src/projePack/Main.java
@@ -1,28 +1,74 @@
 package projePack;
-import projePack.UI;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import javax.swing.*;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         person person = new person();
         relation relation = new relation();
+        Scanner scan = new Scanner(System.in);
+       boolean startUp = true;
+       while (startUp) {
+           startUp=true;
+           entranceMenu();
+           String inputForMenu = scan.nextLine();
+           switch (inputForMenu) {
+               case "1":
+                   boolean startForAddPerson = true;
+                   while (startForAddPerson) {
+                       startForAddPerson = true;
+                       System.out.println("Enter 1 to continue adding \n Enter 2 to add to person 0\n Enter 3 to add to person 0 \nEnter 0 to stop adding");
+                       String inputForAdd = scan.nextLine();
+                       if (person.persons.isEmpty()) {
+                           System.out.println("Ilk kisi ekleme ");
+                           person.personAdder();
+                           break;
+                       }
+                       else {
+                           System.out.println("Press 1 to add mother and father");
+                           System.out.println("Press 2 to add partner");
+                           System.out.println("Press 3 to add brother");
+                           System.out.println("Press 4 to add children");
+                           String inputForRelType = scan.nextLine();
+                           switch (inputForRelType) {
+                               case "1":
+                                   person.personAdder(); //spouse 1 == father
+                                   person.personAdder(); //spouse 2 == mother
+                                   relation.relationAdder(); //spouse1 and spouse 2 relation
+                                   break;
+                               case "2":
+                                   person.personAdder(); // partner
+                                   relation.relationAdder(); // person 0 and relation
+                                   break;
 
-        System.out.println("Kişi ekleme hoş geldiniz.");
-        person.personAdder();//ilk kisi eklendi id=0
+                           }
 
-        person.personAdder();//babam id=1
-        person.personAdder();//annem id=2
-        relation.relationAdder(person.persons.get(0),person.persons.get(1)); //relid = 0
-        person.childrenAdder(relation,0,0);
-
-        person.personAdder();//amcam id=3
-        person.childrenAdder(relation,1,5);
+                       }
 
 
 
+                   }
+                   break;
+               case "2":
+
+                   break;
+
+               case "3":
+
+                   break;
+
+               case "4":
+                   break;
+           }
+       }
+
+
+    }
+
+    public static void entranceMenu() {
+        System.out.println("Kişi eklemek için 1'e basın ");
+        System.out.println("Kişi bilgisi güncellemek / silmek için 2'ye basın ");
+        System.out.println("Devam etmek için herhangi bir tuşa basın");
+        System.out.println("Kapatmak için 0'a basın\n\nGiriniz: ");
     }
 }
--- src/projePack/relation.java
+++ src/projePack/relation.java
@@ -1,8 +1,10 @@
 package projePack;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class relation {
+    person person;
     projePack.person spouse1;
     projePack.person spouse2;
     static int relCounter = 1;
@@ -14,14 +16,35 @@ public class relation {
     ArrayList<projePack.person> mother = new ArrayList<projePack.person>();
     ArrayList<projePack.person> partner = new ArrayList<projePack.person>();
     ArrayList<projePack.person> brother = new ArrayList<projePack.person>();
+    Scanner scan = new Scanner(System.in);
 
     public relation(){
 
     }
 
-    public void relationAdder(projePack.person person1, projePack.person person2){
-        relations.add(new relation(person1,person2));
+    public void relationAdder() {
+
+        System.out.println("Enter spouse 1's id");
+        int spouse1ID = scan.nextInt();
+        System.out.println("Enter spouse 2's id");
+        int spouse2ID = scan.nextInt();
+
+        for (int i = 0;i<person.persons.size();i++) {
+            if (!(person.persons.get(i).id == spouse1ID)) {
+                System.out.println("Cant found");
+                return;
+            }
+        }
+        for (int i = 0;i<person.persons.size();i++) {
+            if (!(person.persons.get(i).id == spouse2ID)) {
+                System.out.println("Cant found");
+                return;
+            }
+        }
+        relations.add(new relation(person.persons.get(spouse1ID),person.persons.get(spouse2ID)));
+
     }
+
 
 
     public relation(projePack.person spouse1, projePack.person spouse2) {
